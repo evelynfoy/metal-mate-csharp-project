4750b63407f5de4075bd0a692efd11980be20464
Evelyn Foy <evelyn.foy@hotmail.com>
2026-07-13 20:56:29 +0100

    #1 - Added HomeViewModel class

---
+++ Metal Mate MVC/Metal Mate MVC/Models/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Metal_Mate_MVC.Models
+{
+    public class HomeViewModel
+    {
+        public SpotPrice? SpotPrice { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
