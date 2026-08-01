c6c95442db31686412efdd502ce0f9f82bcd78cb
Evelyn Foy <evelyn.foy@hotmail.com>
2026-08-01 18:34:24 +0100

    #5 - Added a model for the Edit Profile View

---
+++ Metal Mate MVC/Metal Mate MVC/Models/EditProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Metal_Mate_MVC.Models
+{
+    public class EditProfileViewModel
+    {
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string FavouriteMetal { get; set; } = string.Empty;
+
+        public string? FavouriteCurrency { get; set; } = string.Empty;
+    }
+}
