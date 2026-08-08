b5e0974721cfd2422b38e1732cbe5a1a3f678b29
Evelyn Foy <evelyn.foy@hotmail.com>
2026-08-08 17:18:21 +0100

    #5 - Added default value to favourites

--- Metal Mate MVC/Metal Mate MVC/Models/ApplicationUser.cs
+++ Metal Mate MVC/Metal Mate MVC/Models/ApplicationUser.cs
@@ -6,8 +6,7 @@ namespace Metal_Mate_MVC.Models
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public string FavouriteMetal { get; set; } = string.Empty;
-        public string FavouriteCurrency { get; set; } = string.Empty;
-
+        public string FavouriteMetal { get; set; } = "XAU";
+        public string FavouriteCurrency { get; set; } = "EUR";
     }
 }
